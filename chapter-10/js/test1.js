9aa46c627c7c7a9d824198f70190b53c7fe0e523
lsz <lishengz.cool@hotmail.com>
2016-03-28 22:25:34 +0800

    10.2

--- chapter-10/js/test1.js
+++ chapter-10/js/test1.js
@@ -2,38 +2,35 @@ function  setPos() {
     var pNode = document.getElementById('message');
     pNode.style.position = 'absolute';
     pNode.style.left = '100px';
-    pNode.style.top  = '80px';
+    pNode.style.top  = '50px';
 }
 
-function movStep(params) {
-    
-}
-
-function  moveMes() {
+function  moveMessage() {
     var move = setInterval(function () {
         var pNode = document.getElementById('message');
         var leftPos = parseInt(pNode.style.left);
         var topPos = parseInt(pNode.style.top);
         
-        if (leftPos > 200) {
+        if (leftPos === 200 && topPos === 100) {
+            clearInterval(move);    
+        }
+                
+        if (leftPos > 400) {
             --leftPos;
-        }else {
+        }else if (leftPos < 400){
             ++leftPos;
         }
         
-        if (topPos > 100) {
+        if (topPos > 200) {
             --topPos;
-        }else{
+        }else if (topPos < 200){
             ++topPos;
         }
         pNode.style.left = leftPos + 'px';
         pNode.style.top  = topPos + 'px';
-        
-        if (leftPos === 200 && topPos === 100) {
-            clearInterval(move);    
-        }
-        
-    }, 100);
+                
+    }, 50);
 }
 
 loadEvent(setPos);
+loadEvent(moveMessage);
